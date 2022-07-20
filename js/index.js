131325f17f32ded4bd28545e9bbf1556305cfc64
dav24github <davidthbsossa@gmail.com>
2022-07-19 23:11:06 -0400

    another commit

--- js/index.js
+++ js/index.js
@@ -4,3 +4,29 @@ const line = document.querySelectorAll(".line-section");
 line.forEach((line) => {
   line.style.width = line.parentElement.getBoundingClientRect().height + "px";
 });
+
+// ==================== Line ====================
+ScrollReveal().reveal(".image-svg", {
+  duration: 300,
+  delay: 500,
+  afterReveal: function (el) {
+    el.classList.add("is-visible");
+  },
+  beforeReset: function (el) {
+    el.classList.remove("is-visible");
+  },
+});
+
+ScrollReveal().reveal(".header__main-nav li", {
+  distance: "100px",
+  origin: "top",
+  duration: 600,
+  interval: 100,
+});
+
+ScrollReveal().reveal(".hero__social div", {
+  distance: "100px",
+  origin: "left",
+  duration: 600,
+  interval: 100,
+});
